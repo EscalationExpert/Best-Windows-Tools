15d06339d8d4eec0b8d04246f0c67abac6b90b18
EscalationExpert <131571636+EscalationExpert@users.noreply.github.com>
2023-07-07 16:28:39 +0200

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,3 +1,7 @@
 # This batch file sets up a lot of useful tools.
 ECHO OFF
-winget install wingetui chocolatey vscode nvm 1password chrome firefox discord steam VideoLAN.VLC git cmder 
+winget install wingetui chocolatey vscode nvm 1password chrome firefox discord steam VideoLAN.VLC git cmder rainmeter
+JaxCoreSetup.bat
+git clone https://github.com/Droptop-Four/Basic-Version.git
+cd Basic-Version
+"Droptop Basic Version.rmskin"
