399c05219f4d43fd0bff9166db8ce5a042dc2367
EscalationExpert <131571636+EscalationExpert@users.noreply.github.com>
2023-07-08 19:27:38 +0200

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,8 @@
 # This batch file sets up a lot of useful tools.
 ECHO OFF
-winget install wingetui chocolatey vscode nvm 1password chrome firefox discord steam VideoLAN.VLC git cmder rainmeter
+# old version,  too manual
+#  winget install wingetui chocolatey vscode nvm 1password chrome firefox discord steam VideoLAN.VLC git cmder rainmeter
+winget import ./wingetui-export.json # see json file for detailed overview of tools that will be setuped
 JaxCoreSetup.bat
 git clone https://github.com/Droptop-Four/Basic-Version.git
 cd Basic-Version
